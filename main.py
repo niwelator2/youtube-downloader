9fc4ca1b5721135419541eb2cf39f5066eb46e12
FilipP <niwelator2@gmail.com>
2023-08-19 12:03:05 +0200

    Suport for .exe

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 from pytube import YouTube
 from tqdm import tqdm
+import tkinter as tk
+from tkinter import Label, Entry, Button, OptionMenu, StringVar, filedialog
 
 def on_progress(stream, chunk, bytes_remaining):
     # Calculate the percentage of completion
@@ -10,26 +12,33 @@ def on_progress(stream, chunk, bytes_remaining):
     tqdm_instance.n = percent
     tqdm_instance.refresh()
 
-def download_video(link, download_type, save_directory):
+def download_video():
+    link = link_entry.get()
+    download_type = download_type_var.get()
+    save_directory = save_directory_entry.get()
+
+    if not os.path.exists(save_directory):
+        os.makedirs(save_directory)
+
     try:
         youtube_object = YouTube(link, on_progress_callback=on_progress)
         video_title = youtube_object.title
 
-        if download_type == 'mp4':
+        if download_type == 'MP4':
             stream = youtube_object.streams.get_highest_resolution()
             print("Downloading video:", video_title)
             video_file = stream.download(output_path=save_directory, filename=video_title)
             new_file = os.path.join(save_directory, f"{video_title}.mp4")
             os.rename(video_file, new_file)
 
-        elif download_type == 'mp3':
+        elif download_type == 'MP3':
             stream = youtube_object.streams.filter(only_audio=True).first()
             print("Downloading audio (MP3):", video_title)
             audio_file = stream.download(output_path=save_directory, filename=video_title)
             new_file = os.path.join(save_directory, f"{video_title}.mp3")
             os.rename(audio_file, new_file)
 
-        elif download_type == 'audio':
+        elif download_type == 'Audio':
             stream = youtube_object.streams.filter(only_audio=True).first()
             print("Downloading audio (without conversion):", video_title)
             audio_file = stream.download(output_path=save_directory, filename=video_title + ".mp4")
@@ -37,22 +46,44 @@ def download_video(link, download_type, save_directory):
             os.rename(audio_file, new_file)
 
         else:
-            print("Invalid download type. Choose 'mp4', 'mp3', or 'audio'.")
+            print("Invalid download type. Choose 'MP4', 'MP3', or 'Audio'.")
             return
 
         print("Download completed!")
     except Exception as e:
         print("An error has occurred:", str(e))
 
-if __name__ == "__main__":
-    link = input("Enter the YouTube video URL: ")
-    download_type = input("Choose download type ('mp4', 'mp3', or 'audio'): ").lower()
-    save_directory = input("Enter the directory to save the downloaded files: ")
+def select_save_directory():
+    directory = filedialog.askdirectory()
+    save_directory_entry.delete(0, tk.END)
+    save_directory_entry.insert(0, directory)
 
-    if not os.path.exists(save_directory):
-        os.makedirs(save_directory)
+# Create the main window
+window = tk.Tk()
+window.title("YouTube Downloader")
+window.geometry('640x240')
+window.configure(bg='#896848')
 
-    # Create the tqdm progress bar instance
-    tqdm_instance = tqdm(total=100, unit='%', ascii=True)
-    
-    download_video(link, download_type, save_directory)
+# Create and pack GUI elements
+Label(window, text="YouTube Video URL:").pack()
+link_entry = Entry(window, width=50)
+link_entry.pack()
+
+Label(window, text="Download Type:").pack()
+download_type_var = StringVar(window)
+download_type_var.set("MP4")
+download_type_menu = OptionMenu(window, download_type_var, "MP4", "MP3", "Audio")
+download_type_menu.pack()
+
+Label(window, text="Save Directory:").pack()
+save_directory_entry = Entry(window, width=50)
+save_directory_entry.pack()
+
+Button(window, text="Select Directory", command=select_save_directory).pack()
+Button(window, text="Download", command=download_video).pack()
+
+# Create the tqdm progress bar instance
+tqdm_instance = tqdm(total=100, unit='%', ascii=True)
+
+# Start the Tkinter main loop
+window.mainloop()
