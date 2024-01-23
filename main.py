28d1e884ed7067a04fcad06c7ee9a711ee616a94
FilipP <niwelator2@gmail.com>
2024-01-23 20:58:39 +0100

    Start making playlist module

--- main.py
+++ main.py
@@ -1,74 +1,92 @@
 import os
-from pytube import YouTube
+from pytube import YouTube, Playlist
 import tkinter as tk
 from tkinter import ttk, Label, Entry, Button, OptionMenu, StringVar, filedialog
+
 
 def on_progress(stream, chunk, bytes_remaining):
     bytes_downloaded = stream.filesize - bytes_remaining
     percent = (bytes_downloaded / stream.filesize) * 100
     update_progress_bar(percent)
 
+
 def update_progress_bar(percent):
     progress_label.config(text=f"Progress: {percent:.2f}%")
 
-def clean_video_title(title):
-    # Replace invalid characters in the video title
-    return "".join(c if c.isalnum() or c in [' ', '_', '-'] else '_' for c in title)
 
-def download_video():
-    link = link_entry.get()
-    download_type = download_type_var.get()
-    save_directory = save_directory_entry.get()
+def clean_video_title(title):
+    return "".join(c if c.isalnum() or c in [" ", "_", "-"] else "_" for c in title)
+
+
+def download_single_video(youtube_object, download_type, save_directory):
+    video_title = clean_video_title(youtube_object.title)
+
+    if download_type == "MP4":
+        stream = youtube_object.streams.get_highest_resolution()
+        print("Downloading video:", video_title)
+        video_file = stream.download(output_path=save_directory, filename=video_title)
+        new_file = os.path.join(save_directory, f"{video_title}.mp4")
+        os.rename(video_file, new_file)
+
+    elif download_type == "MP3":
+        stream = youtube_object.streams.filter(only_audio=True).first()
+        print("Downloading audio (MP3):", video_title)
+        audio_file = stream.download(output_path=save_directory, filename=video_title)
+        new_file = os.path.join(save_directory, f"{video_title}.mp3")
+        os.rename(audio_file, new_file)
+
+    else:
+        print("Invalid download type. Choose 'MP4' or 'MP3'.")
+        return
+
+    print(f"Download of {video_title} completed!")
+
+
+def download_playlist_videos(playlist_url, save_directory):
+    playlist = Playlist(playlist_url)
+
+    if not playlist.video_urls:
+        print("Playlist is empty or invalid.")
+        return
+
+    if not save_directory:
+        save_directory = playlist.title()
 
     if not os.path.exists(save_directory):
         os.makedirs(save_directory)
 
-    try:
-        YouTube.allow_oauth_cache = True
-        youtube_object = YouTube(link, on_progress_callback=on_progress)
-        video_title = clean_video_title(youtube_object.title)
+    print(f"Downloading playlist: {playlist.title()}")
 
-        if download_type == "MP4":
-            stream = youtube_object.streams.get_highest_resolution()
-            print("Downloading video:", video_title)
-            video_file = stream.download(
-                output_path=save_directory, filename=video_title
-            )
-            new_file = os.path.join(save_directory, f"{video_title}.mp4")
-            os.rename(video_file, new_file)
+    for video_url in playlist.video_urls:
+        try:
+            video = YouTube(video_url, on_progress_callback=on_progress)
+            download_single_video(video, download_type_var.get(), save_directory)
+        except Exception as e:
+            print(f"Error downloading video {video_url}: {str(e)}")
 
-        elif download_type == "MP3":
-            stream = youtube_object.streams.filter(only_audio=True).first()
-            print("Downloading audio (MP3):", video_title)
-            audio_file = stream.download(
-                output_path=save_directory, filename=video_title
-            )
-            new_file = os.path.join(save_directory, f"{video_title}.mp3")
-            os.rename(audio_file, new_file)
+    print("Playlist download completed!")
 
-        elif download_type == "Audio":
-            stream = youtube_object.streams.filter(only_audio=True).first()
-            print("Downloading audio (without conversion):", video_title)
-            audio_file = stream.download(
-                output_path=save_directory, filename=video_title + ".mp4"
-            )
-            new_file = os.path.join(save_directory, f"{video_title}.mp3")
-            os.rename(audio_file, new_file)
 
-        else:
-            print("Invalid download type. Choose 'MP4', 'MP3', or 'Audio'.")
-            return
+def download_video():
+    link = link_entry.get()
+    save_directory = save_directory_entry.get()
 
-        print("Download completed!")
-    except Exception as e:
-        print(f"An error has occurred: {str(e)}")
-        # Provide a user-friendly error message
+    if "playlist?list=" in link:
+        download_playlist_videos(link, save_directory)
+    else:
+        download_single_video(
+            YouTube(link, on_progress_callback=on_progress),
+            download_type_var.get(),
+            save_directory,
+        )
+
 
 def select_save_directory():
     directory = filedialog.askdirectory()
     if directory:
         save_directory_entry.delete(0, tk.END)
         save_directory_entry.insert(0, directory)
+
 
 # Create the main window
 window = tk.Tk()
@@ -89,7 +107,7 @@ link_entry.pack()
 Label(window, text="Download Type:").pack()
 download_type_var = StringVar(window)
 download_type_var.set("MP4")
-download_type_menu = OptionMenu(window, download_type_var, "MP4", "MP3", "Audio")
+download_type_menu = OptionMenu(window, download_type_var, "MP4", "MP3")
 download_type_menu.pack()
 
 Label(window, text="Save Directory:").pack()
