4995a8f17d7e4c887b481b9057ebb940ddb349a0
FilipP <niwelator2@gmail.com>
2024-03-17 20:57:38 +0100

    Rollback before mp3 metadata function

--- src/main.py
+++ src/main.py
@@ -18,7 +18,9 @@ from tkinter import (
 # Parametr to update progress bar
 update_interval = 1
 # Create a queue for message display
+# Create a queue for message display
 message_queue = queue.Queue()
+download_queue = queue.Queue()
 
 
 def display_message(message, video_title):
@@ -26,9 +28,6 @@ def display_message(message, video_title):
 
     # Start a separate thread to handle displaying messages
     threading.Thread(target=display_messages_from_queue).start()
-
-
-# Def that colect all meseges form system and add them in to queue to prevent freezeing app
 
 
 def display_messages_from_queue():
@@ -44,9 +43,6 @@ def display_messages_from_queue():
         message_queue.task_done()
 
 
-# This def tells how much buytes of the song is left to dowloand
-
-
 def on_progress(stream, chunk, bytes_remaining, current_video):
     bytes_downloaded = stream.filesize - bytes_remaining
     percent = (bytes_downloaded / stream.filesize) * 100
@@ -58,9 +54,6 @@ def update_progress_bar(percent, current_video):
     progress_bar["value"] = percent
     progress_label.config(text=f"Progress: {percent:.2f}% (Video {current_video})")
     window.update_idletasks()
-
-
-# This def extract from url a name of the song/songs
 
 
 def clean_video_title(title):
@@ -128,18 +121,32 @@ def download_single_video(
         show_error_message(error_message)
 
 
-def download_playlist(playlist_link, download_type, save_directory):
-    current_video = 1
-    playlist = Playlist(playlist_link)
-    total_videos = len(playlist.video_urls)
+def download_playlist_threaded(playlist_link, download_type, save_directory):
+    try:
+        playlist_download_thread = threading.Thread(
+            target=start_download_playlist_threaded_inner,
+            args=(playlist_link, download_type, save_directory),
+        )
+        playlist_download_thread.start()
 
-    for video_url in playlist.video_urls:
-        download_single_video(video_url, download_type, save_directory, current_video)
-        current_video += 1
-        percent_complete = (current_video / total_videos) * 100
-        update_progress_bar(percent_complete, current_video)
+        # Schedule check for updates in GUI from the main thread
+        window.after(1000, lambda: check_download_progress(save_directory))
+    except Exception as e:
+        error_message = f"An error has occurred: {str(e)}"
+        show_error_message(error_message)
 
-    display_message("Playlist download completed!", "")
+
+def check_download_progress(save_directory):
+    if os.listdir(save_directory):
+        display_message("Playlist download completed!", "")
+    else:
+        # Schedule another check after 1 second
+        window.after(1000, lambda: check_download_progress(save_directory))
+
+
+def add_to_queue(link, download_type, save_directory):
+    download_queue.put((link, download_type, save_directory))
+    display_message(f"Link add to queue: {link}", "")
 
 
 def select_save_directory(entry_widget, initial_dir=None):
@@ -166,18 +173,20 @@ def load_last_directory():
 def download_single_video_threaded(link, download_type, save_directory, current_video):
     try:
         download_single_video(link, download_type, save_directory, current_video, set())
+        if not download_queue.empty():
+            start_next_download_from_queue()
     except Exception as e:
         error_message = f"An error has occurred: {str(e)}"
         show_error_message(error_message)
 
 
-def download_playlist_threaded(playlist_link, download_type, save_directory):
+def start_next_download_from_queue():
     try:
-        playlist_download_thread = threading.Thread(
-            target=start_download_playlist_threaded_inner,
-            args=(playlist_link, download_type, save_directory),
-        )
-        playlist_download_thread.start()
+        link, download_type, save_directory, current_video = download_queue.get()
+        download_single_video(link, download_type, save_directory, current_video, set())
+        download_queue.task_done()
+        if not download_queue.empty():
+            start_next_download_from_queue()
     except Exception as e:
         error_message = f"An error has occurred: {str(e)}"
         show_error_message(error_message)
@@ -214,7 +223,6 @@ def setup_gui():
     progress_var = DoubleVar()
     progress_label = Label(window, text="Progress: 0.00%")
     progress_label.pack(pady=10)
-
     # Create a text area for system message
     text_area = tk.Text(window, wrap=tk.WORD, state=tk.DISABLED, height=3, width=20)
     text_area.pack(fill=tk.BOTH, expand=True)
@@ -235,7 +243,6 @@ def setup_gui():
     Label(left_frame, text="Save Directory:").pack()
     save_directory_entry = Entry(left_frame, width=50)
     save_directory_entry.pack()
-
     select_directory_button = Button(
         left_frame,
         text="Select Directory",
